87c8a144797ffee444c7a46b9e90f4cb7d19f7be
baboo <baboo.wg@gmail.com>
2015-08-11 18:33:26 +0800

    add base64 url support

--- app.pl
+++ app.pl
@@ -1,9 +1,9 @@
-#!/usr/bin/env perl 
+#!/usr/bin/env perl
 #===============================================================================
 #
 #         FILE: app.pl
 #
-#        USAGE: ./app.pl  
+#        USAGE: ./app.pl
 #
 #  DESCRIPTION: js/css文件合并代理服务，支持版本和缓存管理，自动压缩
 #
@@ -12,7 +12,7 @@
 #         BUGS: ---
 #        NOTES: ---
 #       AUTHOR: Baboo (8boo.net), baboo.wg@gmail.com
-# ORGANIZATION: 
+# ORGANIZATION:
 #      VERSION: 1.0
 #      CREATED: 2015/05/27 17时02分12秒
 #     REVISION: ---
@@ -24,6 +24,7 @@ use Mojo::URL;
 use Mojo::Util qw/decode/;
 use CSS::Minifier;
 use JavaScript::Minifier;
+use MIME::Base64::URLSafe;
 use utf8;
 
 my $ua = Mojo::UserAgent->new(
@@ -36,9 +37,9 @@ my %cache = ();
 
 get '/proxy/cache_list' => sub {
     my $c = shift;
-   
+
     my $json = {};
-    
+
     for my $key (keys %cache) {
         my $remain_time = $cache{$key}{expire_time} - time;
         next if $remain_time < 0;
@@ -51,23 +52,34 @@ get '/proxy/cache_list' => sub {
 };
 
 get '/proxy/#type/*resource_page' => [ type => qr/(?:js|css)(?:\.\w+)?(?:\.\d+)?/i ] => sub {
-    my $c = shift; 
-    
-    my $url = Mojo::URL->new('http://' . $c->param('resource_page'));
+    my $c = shift;
+
     my $config_info = $c->param('type');
 
     my ($type, $version, $cache_time) = split /\./, $config_info;
-    
+
     $cache_time //= 3600;
+
+    my $resource_page = $c->param('resource_page');
+
+    if ($version =~ m/^b64/) {
+        $resource_page = urlsafe_b64decode($resource_page);
+    }
+
+    $resource_page = "http://$resource_page" unless $resource_page ~~ /^http/i;
+
+    my $direct_url = $resource_page =~ m/\Q.$type\E$/i;
+
+    my $url = Mojo::URL->new($resource_page);
 
     my $cache_key = "$type.$version/$url";
 
-    my ($tag_name, $attr_name, $content_type, $minify) = $type eq 'js' 
-        ? ('script', 'src', 'application/javascript;charset=utf8', \&JavaScript::Minifier::minify) 
+    my ($tag_name, $attr_name, $content_type, $minify) = $type eq 'js'
+        ? ('script', 'src', 'application/javascript;charset=utf8', \&JavaScript::Minifier::minify)
         : ('link', 'href', 'text/css;charset=utf8', \&CSS::Minifier::minify);
 
-    if ($cache{$cache_key} 
-        && time - $cache{$cache_key}{time} <= $cache_time 
+    if ($cache{$cache_key}
+        && time - $cache{$cache_key}{time} <= $cache_time
         && time < $cache{$cache_key}{expire_time}
     ) {
         $c->res->headers->content_type($content_type);
@@ -84,17 +96,25 @@ get '/proxy/#type/*resource_page' => [ type => qr/(?:js|css)(?:\.\w+)?(?:\.\d+)?
     $ua->get($url => sub {
 
         my ($ua, $tx) = @_;
-        
+
         my $res = $tx->success;
 
         unless ($res) {
             return $c->render(text => "Resource page load fail:[$url]", status => 404);
         }
 
-        my $dom = $res->dom;
         my @all_content = ();
         my $request_count = 0;
-        my @fail_resources = (); 
+        my @fail_resources = ();
+
+        if ($direct_url) {
+            return response(
+                $c, $page_tx, \@fail_resources, $res->body,
+                $content_type, $cache_key, $cache_time
+            );
+        }
+
+        my $dom = $res->dom;
 
         $dom->find($tag_name)->each(sub {
             my ($elem, $num) = @_;
@@ -102,14 +122,14 @@ get '/proxy/#type/*resource_page' => [ type => qr/(?:js|css)(?:\.\w+)?(?:\.\d+)?
             if (my $src = $elem->attr($attr_name)) {
                 return if $src ~~ m{/proxy/};
 
-                my $resource_url = Mojo::URL->new($src)->to_abs($url); 
+                my $resource_url = Mojo::URL->new($src)->to_abs($url);
 
                 $request_count++;
 
                 $c->app->log->debug("Load $resource_url");
                 $ua->get($resource_url, sub {
                     my ($ua, $tx) = @_;
-                    
+
                     $c->app->log->debug("Load ok $resource_url");
                     if (my $res = $tx->success) {
                         my $body = $res->body;
@@ -125,8 +145,9 @@ get '/proxy/#type/*resource_page' => [ type => qr/(?:js|css)(?:\.\w+)?(?:\.\d+)?
                         if ($type eq 'css') {
                             $body =~ s{url\((.+?)\)}{
                                 my $src = $1;
+                                $src =~ s/(^['"]|['"]$)//g;
                                 unless ($src ~~ /^http/) {
-                                    $src = Mojo::URL->new($src)->to_abs($resource_url) . ''; 
+                                    $src = Mojo::URL->new($src)->to_abs($resource_url) . '';
                                 }
                                 "url($src)";
                             }xsemgi;
@@ -141,17 +162,17 @@ get '/proxy/#type/*resource_page' => [ type => qr/(?:js|css)(?:\.\w+)?(?:\.\d+)?
 
                     if (--$request_count == 0) {
                         return response(
-                            $c, $page_tx, \@fail_resources, join("\n", @all_content), 
+                            $c, $page_tx, \@fail_resources, join("\n", @all_content),
                             $content_type, $cache_key, $cache_time
                         );
                     }
                 });
-            } 
+            }
         });
 
         if ($request_count == 0) {
             return response(
-                $c, $page_tx, \@fail_resources, join("\n", @all_content), 
+                $c, $page_tx, \@fail_resources, join("\n", @all_content),
                 $content_type, $cache_key, $cache_time
             );
         }
@@ -164,7 +185,7 @@ sub response {
     my ($c, $tx, $fail_resources, $content, $content_type, $cache_key, $cache_time) = @_;
 
     if (@$fail_resources) {
-        my $warning = sprintf("/*Warning: There %d resource load fail:%s*/", 
+        my $warning = sprintf("/*Warning: There %d resource load fail:%s*/",
             scalar(@$fail_resources), join(",", @$fail_resources)
         );
 
